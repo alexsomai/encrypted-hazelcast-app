17ea803458141555aa40fb255a0955b9b4f5a7c5
Alexandru Somai <somai.alexandru@gmail.com>
2018-04-04 17:00:27 +0300

    Rewrite EncryptedHazelcastCacheManager by extending HazelcastCacheManager

--- src/main/java/com/alexsomai/blog/cache/EncryptedHazelcastCacheManager.java
+++ src/main/java/com/alexsomai/blog/cache/EncryptedHazelcastCacheManager.java
@@ -4,43 +4,20 @@ import com.alexsomai.blog.crypto.CipherService;
 import com.hazelcast.core.DistributedObject;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
 import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
-import org.springframework.util.Assert;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-public class EncryptedHazelcastCacheManager implements CacheManager {
-
-    /**
-     * Property name for hazelcast spring-cache related properties
-     */
-    public static final String CACHE_PROP = "hazelcast.spring.cache.prop";
+public class EncryptedHazelcastCacheManager extends HazelcastCacheManager {
 
     private final ConcurrentMap<String, Cache> caches = new ConcurrentHashMap<>();
     private HazelcastInstance hazelcastInstance;
     private CipherService cipherService;
-
-    /**
-     * Default cache value retrieval timeout. Apply to all caches.
-     * Can be overridden setting a cache specific value to readTimeoutMap.
-     */
-    private long defaultReadTimeout;
-    /**
-     * Holds cache specific value retrieval timeouts. Override defaultReadTimeout for specified caches.
-     */
-    private Map<String, Long> readTimeoutMap = new HashMap<>();
-
-    public EncryptedHazelcastCacheManager() {
-    }
 
     public EncryptedHazelcastCacheManager(HazelcastInstance hazelcastInstance, CipherService cipherService) {
         this.hazelcastInstance = hazelcastInstance;
@@ -78,73 +55,6 @@ public class EncryptedHazelcastCacheManager implements CacheManager {
 
     private long calculateCacheReadTimeout(String name) {
         Long timeout = getReadTimeoutMap().get(name);
-        return timeout == null ? defaultReadTimeout : timeout;
-    }
-
-    public HazelcastInstance getHazelcastInstance() {
-        return hazelcastInstance;
-    }
-
-    public void setHazelcastInstance(final HazelcastInstance hazelcastInstance) {
-        this.hazelcastInstance = hazelcastInstance;
-    }
-
-    private void parseOptions(String options) {
-        if (null == options || options.trim().isEmpty()) {
-            return;
-        }
-        for (String option : options.split(",")) {
-            parseOption(option);
-        }
-    }
-
-    private void parseOption(String option) {
-        String[] keyValue = option.split("=");
-        Assert.isTrue(keyValue.length != 0, "blank key-value pair");
-        Assert.isTrue(keyValue.length <= 2, String.format("key-value pair %s with more than one equals sign", option));
-
-        String key = keyValue[0].trim();
-        String value = (keyValue.length == 1) ? null : keyValue[1].trim();
-
-        Assert.isTrue(value != null && !value.isEmpty(), String.format("value for %s should not be null or empty", key));
-
-        if ("defaultReadTimeout".equals(key)) {
-            defaultReadTimeout = Long.parseLong(value);
-        } else {
-            readTimeoutMap.put(key, Long.parseLong(value));
-        }
-    }
-
-    /**
-     * Return default cache value retrieval timeout in milliseconds.
-     */
-    public long getDefaultReadTimeout() {
-        return defaultReadTimeout;
-    }
-
-    /**
-     * Set default cache value retrieval timeout. Applies to all caches, if not defined a cache specific timeout.
-     * @param defaultReadTimeout default cache retrieval timeout in milliseconds. 0 or negative values disable timeout.
-     */
-    public void setDefaultReadTimeout(long defaultReadTimeout) {
-        this.defaultReadTimeout = defaultReadTimeout;
-    }
-
-    /**
-     * Return cache-specific value retrieval timeouts. Map keys are cache names, values are cache retrieval timeouts in
-     * milliseconds.
-     */
-    public Map<String, Long> getReadTimeoutMap() {
-        return readTimeoutMap;
-    }
-
-    /**
-     * Set the cache ead-timeout params
-     * @param options cache read-timeout params, autowired by Spring automatically by getting value of
-     * hazelcast.spring.cache.prop parameter
-     */
-    @Autowired
-    public void setCacheOptions(@Value("${" + CACHE_PROP + ":}") String options) {
-        parseOptions(options);
+        return timeout == null ? getDefaultReadTimeout() : timeout;
     }
 }
