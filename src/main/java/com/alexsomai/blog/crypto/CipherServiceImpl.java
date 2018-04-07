28aeb35ac7a6233382c5510b33086547b82d3625
Alexandru Somai <somai.alexandru@gmail.com>
2018-04-07 10:23:10 +0300

    Simplify encrypt/decrypt code

--- src/main/java/com/alexsomai/blog/crypto/CipherServiceImpl.java
+++ src/main/java/com/alexsomai/blog/crypto/CipherServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
-import java.text.MessageFormat;
 
 @Component
 public class CipherServiceImpl implements CipherService {
@@ -25,9 +24,9 @@ public class CipherServiceImpl implements CipherService {
     public String hash(Object object) {
         Digest digest = new SHA256Digest();
 
-        final byte[] clearBytes = SerializationUtil.serialize(object);
+        byte[] clearBytes = SerializationUtil.serialize(object);
         digest.update(clearBytes, 0, clearBytes.length);
-        final byte[] hashedBytes = new byte[digest.getDigestSize()];
+        byte[] hashedBytes = new byte[digest.getDigestSize()];
         digest.doFinal(hashedBytes, 0);
 
         return new String(Hex.encode(hashedBytes));
@@ -39,22 +38,8 @@ public class CipherServiceImpl implements CipherService {
             throw new NullPointerException("Cannot encrypt a null value");
         }
 
-        PaddedBufferedBlockCipher bufferedBlockCipher = new PaddedBufferedBlockCipher(new CBCBlockCipher(new AESEngine()));
-
-        KeyParameter keyParameter;
-        keyParameter = new KeyParameter(secretKey.getBytes(StandardCharsets.UTF_8));
-        bufferedBlockCipher.init(true, keyParameter);
-
         byte[] bytesToEncrypt = SerializationUtil.serialize(object);
-        byte[] encryptedTextBytes = new byte[bufferedBlockCipher.getOutputSize(bytesToEncrypt.length)];
-
-        int outputLength = bufferedBlockCipher.processBytes(bytesToEncrypt, 0, bytesToEncrypt.length, encryptedTextBytes, 0);
-
-        try {
-            bufferedBlockCipher.doFinal(encryptedTextBytes, outputLength);
-        } catch (InvalidCipherTextException e) {
-            throw new RuntimeException("Exception occurred while encrypting the object");
-        }
+        byte[] encryptedTextBytes = applyAlgorithm(bytesToEncrypt, true);
 
         return new String(Hex.encode(encryptedTextBytes));
     }
@@ -65,28 +50,33 @@ public class CipherServiceImpl implements CipherService {
             throw new NullPointerException("Cannot decrypt a null value");
         }
 
-        PaddedBufferedBlockCipher bufferedBlockCipher = new PaddedBufferedBlockCipher(new CBCBlockCipher(new AESEngine()));
-
-        byte[] objectToDecrypt = Hex.decode(data);
-        if (objectToDecrypt.length % bufferedBlockCipher.getUnderlyingCipher().getBlockSize() > 0) {
-            String message = MessageFormat.format("After decoding the encrypted object from Base64, " +
-                "the object should contain a number of bytes multiple of {0} and the size of the object in bytes" +
-                " is {1}. Most probably the object has not been encrypted with this library, " +
-                "or maybe it has been tampered", bufferedBlockCipher.getUnderlyingCipher().getBlockSize(), objectToDecrypt.length);
-            throw new IllegalArgumentException(message);
-        }
-
-        KeyParameter keyParameter = new KeyParameter(secretKey.getBytes());
-        bufferedBlockCipher.init(false, keyParameter);
-        final byte[] originalTextBytes = new byte[bufferedBlockCipher.getOutputSize(objectToDecrypt.length)];
-        final int outputLength = bufferedBlockCipher.processBytes(objectToDecrypt, 0, objectToDecrypt.length, originalTextBytes, 0);
-
-        try {
-            bufferedBlockCipher.doFinal(originalTextBytes, outputLength);
-        } catch (InvalidCipherTextException e) {
-            throw new RuntimeException("Exception occurred while decrypting the object");
-        }
+        byte[] bytesToDecrypt = Hex.decode(data);
+        byte[] originalTextBytes = applyAlgorithm(bytesToDecrypt, false);
 
         return SerializationUtil.deserialize(originalTextBytes);
+    }
+
+    private byte[] applyAlgorithm(byte[] inputBytes, boolean forEncryption) {
+        PaddedBufferedBlockCipher paddedBufferedBlockCipher = initPaddedBufferedBlockCipher(forEncryption);
+
+        byte[] outputBytes = new byte[paddedBufferedBlockCipher.getOutputSize(inputBytes.length)];
+        int outputLength = paddedBufferedBlockCipher.processBytes(inputBytes, 0, inputBytes.length, outputBytes, 0);
+
+        try {
+            paddedBufferedBlockCipher.doFinal(outputBytes, outputLength);
+        } catch (InvalidCipherTextException e) {
+            throw new RuntimeException("Exception occurred while encrypting/decrypting the object");
+        }
+
+        return outputBytes;
+    }
+
+    private PaddedBufferedBlockCipher initPaddedBufferedBlockCipher(boolean forEncryption) {
+        PaddedBufferedBlockCipher bufferedBlockCipher = new PaddedBufferedBlockCipher(new CBCBlockCipher(new AESEngine()));
+
+        KeyParameter keyParameter = new KeyParameter(secretKey.getBytes(StandardCharsets.UTF_8));
+        bufferedBlockCipher.init(forEncryption, keyParameter);
+
+        return bufferedBlockCipher;
     }
 }
